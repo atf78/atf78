5a8f17402f2ff33c42f8905d047f269f3962f843
atf78 <alexfabbri20042gmail.com>
2021-11-23 10:19:31 -0500

    sqs code update

--- atf78/hello-world/hello.js
+++ atf78/hello-world/hello.js
@@ -1,29 +1,36 @@
-module.exports.handler = async (event) => {
-  
-  var SQS_QUEUE_URL = 'https://sqs.us-east-2.amazonaws.com/928386612721/mySQSQueue';
-  
-  console.info("EVENT\n" + JSON.stringify(event, null, 2));
-  console.info("Q URL\n " + SQS_QUEUE_URL);
- 
- // console.info('Event: ', event);
-  var params = {
+'use strict';
+var QUEUE_URL = 'https://sqs.us-east-2.amazonaws.com/928386612721/mySQSQueue';
+var AWS = require('aws-sdk');
+var sqs = new AWS.SQS({region :'us-east-2'});
+
+exports.handler = function(event,context,callback) {
+  //exports.handler = function(event, context) {
+    
+  var parms = {
     MessageBody: JSON.stringify(event),  
-    QueueUrl : SQS_QUEUE_URL,
+    QueueUrl : QUEUE_URL,
   };
-
+  sqs.sendMessage(parms, function(err,data){
+    if(err) {
+      console.log('error:',"Fail Send Message" + err);
+      context.done('error',"Error Pit SQS"); // Error w/ message
+    } else {
+      console.log('data:',data.MessageId);
+      context.done(null,''); //Success Sent SQS
+    }  
+  });  
+  
   let responseMessage = 'Hello, World!';
-
   if (event.queryStringParameters && event.queryStringParameters['Name']) {
+    console.info("EVENT new\n" + JSON.stringify(event, null, 2));
+    console.info(`Q URL new\n ${QUEUE_URL}`);
+    console.info("Response\n" + responseMessage);
     responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
   }
 
-  return {
+  callback(null, {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      message: responseMessage,
-    }),
-  }
+    body: JSON.stringify(responseMessage),
+    headers: {'Content-Type': 'application/json'}
+  });
 }
